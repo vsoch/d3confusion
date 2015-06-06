d758f387d08ec86f446ee184ab13e2c421c19ab2
vsochat@stanford.edu <vsochat@stanford.edu>
2015-06-06 16:20:00 -0700

    adding accuracy

--- index.py
+++ index.py
@@ -14,36 +14,51 @@ def get_metrics():
 def get_directions():
     return {"posneg":"Positive and Negative","pos":"Positive Only"}
 
-def get_accuracy():
-   return 99.9
+def get_strategies():
+    return {"cca":"Complete Case Analysis","svi":"Single Value Imputation"}
+
+def get_accuracy(strategy,metric,threshold,direction):
+   df = pandas.read_csv("static/data/supp_data2_ml_accuracy.csv")
+   df = df[df.direction==direction]
+   df = df[df.thresh==int(threshold)]
+   df = df[df.strategy=="%s.%s" %(strategy,metric)]
+   return "%.3f (%.3f,%.3f)" %(df.accuracy_mean.tolist()[0],df.down.tolist()[0],df.up.tolist()[0])
 
 @app.route('/')
 def show_analyses():
 
-   #TODO show accuracy values
-
     metric = "pearson"
     threshold = "1"
+    strategy = "cca"
     direction = "posneg"
     datafile = "confdata.tsv"
+
+    # We will show accuracy values
+    acc = get_accuracy(strategy,metric,threshold,direction)
+
     return render_template('index.html',datafile=datafile,
+                            strategies=get_strategies(),
                             thresholds=get_thresholds(),
                             metrics=get_metrics(),
                             directions=get_directions(),
                             threshold=threshold,
                             direction=direction,
-                            metric=metric)
+                            metric=metric,
+                            strategy=strategy,
+                            acc=acc)
 
-@app.route('/<metric>/<direction>/<threshold>')
-def get_help(metric,direction,threshold):
+@app.route('/<strategy>/<metric>/<direction>/<threshold>')
+def get_help(strategy,metric,direction,threshold):
 
     datafile = "%s_%s_%s.tsv" %(metric,direction,threshold)
     return render_template('index.html',datafile=datafile,
+                            strategies=get_strategies(),
                             thresholds=get_thresholds(),
                             metrics=get_metrics(),
                             directions=get_directions(),
                             threshold=threshold,
                             direction=direction,
+                            strategy=strategy,
                             metric=metric)
 
 if __name__ == '__main__':
