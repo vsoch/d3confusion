fce472e311fcdeb07cfd506b1187a4184e0c863d
vsochat@stanford.edu <vsochat@stanford.edu>
2015-06-07 13:10:09 -0700

    adding about page and final tweaks to interface

--- index.py
+++ index.py
@@ -17,13 +17,16 @@ def get_directions():
 def get_strategies():
     return {"cca":"Complete Case Analysis","svi":"Single Value Imputation"}
 
-def get_accuracy(strategy,metric,threshold,direction):
+def get_accuracy(strategy,metric,direction,threshold):
    df = pandas.read_csv("static/data/supp_data2_ml_accuracy.csv")
    df = df[df.direction==direction]
    df = df[df.thresh==int(threshold)]
    df = df[df.strategy=="%s.%s" %(strategy,metric)]
    return "%.3f (%.3f,%.3f)" %(df.accuracy_mean.tolist()[0],df.down.tolist()[0],df.up.tolist()[0])
 
+@app.route('/about')
+def show_about():
+    return render_template('about.html')
 
 @app.route('/')
 def show_best_analysis():
@@ -35,7 +38,7 @@ def show_best_analysis():
     datafile = "%s_%s_%s_%s.tsv" %(strategy,metric,direction,threshold)
 
     # We will show accuracy values
-    acc = get_accuracy(strategy,metric,threshold,direction)
+    acc = get_accuracy(strategy,metric,direction,threshold)
 
     return render_template('index.html',datafile=datafile,
                             strategies=get_strategies(),
@@ -49,12 +52,12 @@ def show_best_analysis():
                             acc=acc)
 
 @app.route('/<strategy>/<metric>/<direction>/<threshold>')
-def show_specific_analysis(strategy,metric,direction,threshold):
+def show_specific_analysis(strategy,direction,metric,threshold):
 
-    datafile = "%s_%s_%s.tsv" %(strategy,metric,direction,threshold)
+    datafile = "%s_%s_%s_%s.tsv" %(strategy,metric,direction,threshold)
 
     # We will show accuracy values
-    acc = get_accuracy(strategy,metric,threshold,direction)
+    acc = get_accuracy(strategy,metric,direction,threshold)
 
     return render_template('index.html',datafile=datafile,
                             strategies=get_strategies(),
