fdc0eedd4dcd36f7dcd91eb8abe21fc506809ce0
vsochat@stanford.edu <vsochat@stanford.edu>
2015-06-07 11:03:52 -0700

    adding thresholded images

--- index.py
+++ index.py
@@ -24,14 +24,15 @@ def get_accuracy(strategy,metric,threshold,direction):
    df = df[df.strategy=="%s.%s" %(strategy,metric)]
    return "%.3f (%.3f,%.3f)" %(df.accuracy_mean.tolist()[0],df.down.tolist()[0],df.up.tolist()[0])
 
+
 @app.route('/')
-def show_analyses():
+def show_best_analysis():
 
     metric = "pearson"
     threshold = "1"
     strategy = "cca"
     direction = "posneg"
-    datafile = "confdata.tsv"
+    datafile = "%s_%s_%s_%s.tsv" %(strategy,metric,direction,threshold)
 
     # We will show accuracy values
     acc = get_accuracy(strategy,metric,threshold,direction)
@@ -48,9 +49,13 @@ def show_analyses():
                             acc=acc)
 
 @app.route('/<strategy>/<metric>/<direction>/<threshold>')
-def get_help(strategy,metric,direction,threshold):
+def show_specific_analysis(strategy,metric,direction,threshold):
 
-    datafile = "%s_%s_%s.tsv" %(metric,direction,threshold)
+    datafile = "%s_%s_%s.tsv" %(strategy,metric,direction,threshold)
+
+    # We will show accuracy values
+    acc = get_accuracy(strategy,metric,threshold,direction)
+
     return render_template('index.html',datafile=datafile,
                             strategies=get_strategies(),
                             thresholds=get_thresholds(),
--- prep/prep_confmatrix_data.R
+++ prep/prep_confmatrix_data.R
@@ -1,22 +1,41 @@
-datadir = "/home/vanessa/Documents/Work/BRAINMETA/IMAGE_COMPARISON/experiment3_unrelated"
+datadir = "/home/vanessa/Documents/Work/BRAINMETA/IMAGE_COMPARISON/experiment3_unrelated/permutations"
 setwd(datadir)
-load("confmatrix_finished.Rda")
-flat = c()
+input_data = list.files(datadir,pattern="confmatrix*")
+outdir = "/home/vanessa/Documents/Dropbox/Code/Flask/confusion/static"
 
-for (row in 1:nrow(confmatrix)){
-  cat("Parsing row",row,"\n")
-  for (col in 1:ncol(confmatrix)){
-    actual = rownames(confmatrix)[row]
-    predicted = colnames(confmatrix)[col]
-    entry = cbind(actual,predicted,confmatrix[row,col],row,col)
-    flat = rbind(flat,entry)
-  }
+for (i in input_data) {
+	flat = c()
+
+        method = paste(strsplit(i,"_")[[1]][2:3],collapse="_")
+        if (method == "cca_pearson"){
+            confmatrix = confmatrixpd
+        } else if (method == "svi_pearson"){
+            confmatrix = confmatrixpi
+        } else if (method == "cca spearman") {
+            confmatrix = confmatrixsd
+        } else {
+            confmatrix = confmatrixsi
+        }
+
+        direction = strsplit(i,"_")[[1]][4]
+        strategy = strsplit(i,"_")[[1]][2]
+        metric = strsplit(i,"_")[[1]][3]
+        threshold = strsplit(strsplit(i,"_")[[1]][5],"[.]")[[1]][1]
+
+	for (row in 1:nrow(confmatrix)){
+	  cat("Parsing row",row,"\n")
+	  for (col in 1:ncol(confmatrix)){
+	    actual = rownames(confmatrix)[row]
+	    predicted = colnames(confmatrix)[col]
+	    entry = cbind(actual,predicted,confmatrix[row,col],row,col,strategy,metric,direction,threshold)
+	    flat = rbind(flat,entry)
+	  }
+	}
+
+	flat = as.data.frame(flat,stringsAsFactors=FALSE)
+	colnames(flat) = c("actual","predicted","count","x","y","strategy","metric","direction","threshold")
+	write.table(flat,file=paste(outdir,"/",strategy,"_",metric,"_",direction,"_",threshold,".tsv",sep=""),sep="\t",row.names=FALSE,col.names=TRUE,quote=FALSE)
 }
-
-flat = as.data.frame(flat,stringsAsFactors=FALSE)
-colnames(flat) = c("actual","predicted","count","x","y")
-setwd("/home/vanessa/Documents/Dropbox/Code/Javascript/confusion")
-write.table(flat,file="confdata.tsv",sep="\t",row.names=FALSE,col.names=TRUE,quote=FALSE)
 
 labels = c("TASK01_CON07","TASK01_CON08","TASK01_CON09","TASK02_CON25","TASK02_CON26","TASK02_CON27","TASK03_CON19",                   "TASK03_CON20","TASK03_CON22","TASK04_CON61","TASK04_CON62","TASK04_CON63","TASK04_CON64","TASK04_CON65","TASK04_CON66", "TASK04_CON67","TASK04_CON68","TASK04_CON69","TASK04_CON70","TASK04_CON71","TASK04_CON72","TASK04_CON73","TASK05_CON13",                    "TASK05_CON14","TASK05_CON15","TASK06_CON01","TASK06_CON02","TASK06_CON06","TASK07_CON31","TASK07_CON32","TASK07_CON33", "TASK07_CON34","TASK07_CON35","TASK07_CON36","TASK07_CON37","TASK07_CON38","TASK07_CON39","TASK07_CON40","TASK07_CON41", "TASK07_CON45","TASK07_CON46","TASK07_CON47","TASK07_CON48","TASK07_CON49","TASK07_CON50","TASK07_CON51","TASK07_CON52")
           
